c70280d306eab639a963ce042c44e21090dc812b
Juan Manuel Diez <jm10yap@gmail.com>
2022-09-29 15:12:53 -0300

    Agregado de archivo admin y ruta de admin

--- routes/publicRoutes.js
+++ routes/publicRoutes.js
@@ -1,12 +1,7 @@
 const express = require("express");
 const publicRouter = express.Router();
 const pagesController = require("../controllers/pagesController");
-// Rutas Públicas:
-// ...
+
 publicRouter.get("/", pagesController.showHome);
-
-publicRouter.get("/contact", pagesController.showContact);
-
-publicRouter.get("/aboutUs", pagesController.showAboutUs);
 
 module.exports = publicRouter;
