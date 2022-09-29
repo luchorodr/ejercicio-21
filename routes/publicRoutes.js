3fcbeb28f618972c7bc729cb63b2ee4dbea3d6af
Martin Hernandez <martin.hernandez.g@gmail.com>
2022-09-29 14:51:14 -0300

    unaruta

--- routes/publicRoutes.js
+++ routes/publicRoutes.js
@@ -1,7 +1,10 @@
 const express = require("express");
 const publicRouter = express.Router();
-
+const pagesController = require("../controllers/pagesController")
 // Rutas Públicas:
 // ...
+
+
+publicRouter.get("/home", pagesController.showHome )
 
 module.exports = publicRouter;
