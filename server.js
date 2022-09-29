826ed0e07ca2af7d24184490299491030fae07ab
Martin Hernandez <martin.hernandez.g@gmail.com>
2022-09-29 14:02:58 -0300

    aceptenoseq

--- server.js
+++ server.js
@@ -6,7 +6,6 @@ const dbInitialSetup = require("./dbInitialSetup");
 const APP_PORT = process.env.APP_PORT || 3000;
 const app = express();
 
-<<<<<<< HEAD
 app.use(express.static("public"));
 app.use(express.urlencoded({ extended: true }));
 app.set("view engine", "ejs");
@@ -19,9 +18,3 @@ app.listen(APP_PORT, () => {
   console.log(`\n[Express] Servidor corriendo en el puerto ${APP_PORT}.`);
   console.log(`[Express] Ingresar a http://localhost:${APP_PORT}.\n`);
 });
-=======
-app.listen(
-  process.env.APP_PORT,
-  console.log(`Servidor corriendo en el puerto ${process.env.APP_PORT} `)
-);
->>>>>>> 7ede809821e1193fd346fb9db95367a101a17cdd
