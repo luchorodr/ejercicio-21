be4365cf4c216e97c2ca58cb45bf639a504ff091
Juan Manuel Diez <jm10yap@gmail.com>
2022-09-29 14:57:05 -0300

    rutas

--- routes/publicRoutes.js
+++ routes/publicRoutes.js
@@ -1,10 +1,12 @@
 const express = require("express");
 const publicRouter = express.Router();
-const pagesController = require("../controllers/pagesController")
+const pagesController = require("../controllers/pagesController");
 // Rutas Públicas:
 // ...
+publicRouter.get("/", pagesController.showHome);
 
+publicRouter.get("/contact", pagesController.showContact);
 
-publicRouter.get("/home", pagesController.showHome )
+publicRouter.get("/aboutUs", pagesController.showAboutUs);
 
 module.exports = publicRouter;
