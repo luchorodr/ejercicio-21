a5cfc5c6d851e5530045762bb2b995c66aca35b1
Juan Manuel Diez <jm10yap@gmail.com>
2022-09-30 14:40:08 -0300

    modificaciones en nombres y creacion de nuevo articulo

--- controllers/articleController.js
+++ controllers/articleController.js
@@ -10,12 +10,15 @@ async function show(req, res) {}
 async function create(req, res) {}
 
 // Store a newly created resource in storage.
-async function store(req, res) {}
+async function store(req, res) {
+  await Article.create({ title: req.body.crearTitulo, content: req.body.crearContenido });
+  res.redirect("admin");
+}
 
 // Show the form for editing the specified resource.
 async function edit(req, res) {
   const article = await Article.update(
-    { title: req.body.crearTitulo, content: req.body.crearContenido },
+    { title: req.body.modificarTitulo, content: req.body.modificarContenido },
     { where: { id: req.params.id } },
   );
   res.redirect("/admin");
